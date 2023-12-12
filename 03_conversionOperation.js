7c983e15cea883a24138f0ecd09347cd8c544444
kamran3200malik <kamran3200malik@gmail.com>
2023-12-12 18:51:41 +0500

    conversion of dataTypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,39 @@
+/*
+let score="33abc";
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+"33" => Number
+"33ab"=> NaN
+True=>1
+False=>0
+
+*/
+
+/*
+let isLoggedIn="kamran";
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+ 
+1=>True
+0=>False
+"kamran"=>True
+""=>false
+*/
+
+let someNumber=33;
+
+let stringNumber=String(someNumber);
+console.log(someNumber);
+console.log(typeof someNumber);
+console.log(typeof stringNumber);
