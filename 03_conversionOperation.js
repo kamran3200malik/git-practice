e3d392294c4b0ff0a574df5956e526238981f8f4
kamran3200malik <kamran3200malik@gmail.com>
2023-12-12 21:28:11 +0500

    conversion,operation ,increment

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -29,11 +29,68 @@ console.log(booleanIsLoggedIn);
 0=>False
 "kamran"=>True
 ""=>false
-*/
+
 
 let someNumber=33;
 
 let stringNumber=String(someNumber);
 console.log(someNumber);
 console.log(typeof someNumber);
-console.log(typeof stringNumber);
+console.log(typeof stringNumber);*/
+
+// *********************************   Operations *********************************************
+
+/*let value = 3
+let negValue= -value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+
+let str1 = "Hello";
+let str2 = " Kamran Malik"
+let str3 = str1 + str2;
+console.log(str3)
+
+
+console.log ("1"+2); //12
+console.log (1+"2"); //12
+console.log ("1"+2+2); //122
+console.log (1+2+"2"); //32
+console.log ("1"+(2+2)); //14
+
+console.log( 3+4*5%3);
+console.log( (3+4)*5%3);
+
+console.log(true);  //true
+console.log(+true);  // 1
+console.log(+"");  //  0
+
+
+let num1, num2 , num3;
+num1=num2=num3=2+2
+console.log(num1)
+console.log(num2)
+console.log(num3)
+
+let gameCounter =100;
+gameCounter++;
+console.log(gameCounter);    //101 postfix increment
+
+++gameCounter;
+console.log(gameCounter);    //102 prefix increment
+*/
+
+let x=3;
+let y=x++;
+console.log(`x:${x}, y:${y}`);
+
+let a,b;
+a=100;
+b=++a;
+console.log(`a:${a} , b:${b}`);
