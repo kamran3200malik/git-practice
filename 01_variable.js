e45483226c05b71f260ddff331b358db7c1b5d60
kamran3200malik <kamran3200malik@gmail.com>
2023-12-12 16:56:47 +0500

    add javascript file

---
+++ 01_variable.js
@@ -0,0 +1,6 @@
+const accountId="4111";
+let accountEmail="kamranaa@gmail.com"
+var accountPassword="11223344";
+accountCity="kamra";
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
