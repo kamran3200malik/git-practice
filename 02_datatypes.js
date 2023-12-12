52ab393be00c89ef8762bc44fd2915986e4818c4
kamran3200malik <kamran3200malik@gmail.com>
2023-12-12 18:10:26 +0500

    02 lecture datatype

---
+++ 02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; //trat all js code as newer versio
+
+// alert(3+3); //we are using node.js not browser
+
+/*console.log(3 + 3);
+console.log("kamran");
+
+let name="kamran";
+let age = 18;
+let isLoggedIn= false;
+
+console.table([name,age,isLoggedIn])
+
+
+number=> 2 to power 53
+bigint
+string=>""
+boolean =>true/false
+null => stanalone value
+undefined=>
+symbol=>unique
+
+*/
+
+// object
+
+console.log(typeof "kamran")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
+
+
