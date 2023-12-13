69c547135205cc42026b0b59c02fea9ce32d3e58
kamran3200malik <kamran3200malik@gmail.com>
2023-12-13 20:42:38 +0500

    add new math.js file

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,40 @@
+/*const score=600
+console.log(score);
+
+const balance = new Number(1000)
+console.log(balance);  //[Number:1000]
+
+console.log(balance.toString()); //1000
+console.log(balance.toFixed(2)); //1000.00
+console.log(balance.toString().length); //4
+
+
+const otherNumber=123.0945
+console.log(otherNumber.toPrecision(4));   //123.1
+
+const hunderds=1000000
+console.log(hunderds.toLocaleString('en-IN'));
+*/
+
+
+//+++++++++++++++++++++++++++++++++++++++++++  Maths    ++++++++++++++++++++++++
+/*console.log(Math);
+console.log(Math.PI); //3.14
+console.log(Math.abs(-5)); //5
+console.log(Math.round(5.6)); //6
+console.log(Math.ceil(4.2)); //5
+console.log(Math.floor(6.9));   //6
+console.log(Math.min(24,25,29,2,998,22,1,0.25)); //0.25
+console.log(Math.max(1000,2050,20,1,353,256));//2050
+
+
+// console.log(Math.random());
+console.log(Math.round(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+*/
+
+let min=10;
+let max=20;
+console.log(Math.round(Math.random()*(max-min+1))+min)
+
+
