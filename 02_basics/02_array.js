e77f916ae6761ad66fc5650a47b0e1d0d555db79
kamran3200malik <kamran3200malik@gmail.com>
2023-12-13 23:01:18 +0500

    new file 02_array.js added

---
+++ 02_basics/02_array.js
@@ -0,0 +1,16 @@
+const attaki_heros=["kamran","mehran","nouman"]
+
+const kamra_heros=["Affan","Farhan","malik"]
+
+// attaki_heros.push(kamra_heros);
+// console.log(attaki_heros)
+// console.log(attaki_heros[3][1])
+// const allHero= attaki_heros.concat(kamra_heros);
+// console.log(allHero);
+// const allnewHeros=[...attaki_heros,...attaki_heros]
+// console.log(allnewHeros);
+
+/*
+const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array=another_array.flat(Infinity)
+console.log(real_another_array);*/
