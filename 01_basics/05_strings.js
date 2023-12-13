9efc1f6a88efb574d6b347ad2931b06faa2cdf27
kamran3200malik <kamran3200malik@gmail.com>
2023-12-13 19:42:51 +0500

    strings.js added

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,45 @@
-const name="kamran"
+/*const name="kamran"
 const repoCount = 50;
 
-console.log(name + repoCount + " Malik");
+// console.log(name + repoCount + " Malik");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String('kamran')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.toUpperCase());
+console.log(gameName.length);
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('m'));
+
+const gameName=new String('kamran')
+const newString=gameName.substr(0,3)
+console.log(newString);
+
+const anotherString= gameName.slice(-6,2)
+console.log(anotherString);
+
+
+const newStringOne="      kamran          "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url= "https://kamran.com/kamran%20malik";
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('kamran'));
+console.log(url.search('kamran'));
+console.log(url[url.search('kamran')]);
+*/
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+const word=str.split(' ');
+console.log(word[0]);
+console.log(word[1]);
+console.log(word[2]);
+console.log(word[3]);
+
+const strCop=str.split();
+console.log(strCop);
