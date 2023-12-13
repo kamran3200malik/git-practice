51147aa741565747d6f49057b239a73cbe1da505
kamran3200malik <kamran3200malik@gmail.com>
2023-12-13 21:49:07 +0500

    new file date.js added

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,39 @@
+// Dates
+
+const myDate= new Date();
+/*console.log(myDate); //2023-12-13T15:59:22.237Z
+console.log(myDate.toString());  //Wed Dec 13 2023 21:02:15 GMT+0500 (Pakistan Standard Time)
+console.log(myDate.toDateString()); //Wed Dec 13 2023
+console.log(myDate.toISOString()); //  2023-12-13T16:02:15.211Z
+
+console.log(myDate.toLocaleString()); //12/13/2023, 9:05:57 PM
+console.log(myDate.toTimeString()); // 21:08:26 GMT+0500 (Pakistan Standard Time)
+console.log(myDate.toUTCString()); // Wed, 13 Dec 2023 16:08:26 GMT
+
+console.log(myDate.toJSON()); //2023-12-13T16:09:45.171Z
+
+
+let myCreatedDate= new Date(2023, 0, 28)
+let myCreatedDate= new Date(2023, 0, 28 ,5 ,3)
+
+console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDate= new Date("01-15-2023")
+console.log(myCreatedDate.toLocaleString());
+
+
+let myTimestamp =Date.now()
+console.log(myTimestamp);
+
+let newDate=new Date()
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getMonth()+1);
+console.log(newDate.getFullYear());
+console.log(newDate.getHours());
+console.log(newDate.getMinutes());
+
+console.log(`Time is ${newDate.getHours()} : ${newDate.getMinutes()}   );
+
+
+*/
