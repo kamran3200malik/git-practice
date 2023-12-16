4f1ca36dc60da516a76a53a978431e610212edb1
kamran3200malik <kamran3200malik@gmail.com>
2023-12-16 21:59:36 +0500

    new file added

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,26 @@
+/*const userEmail="kamran"
+if (userEmail) {
+    console.log("Got User Email");
+}
+else{
+    console.log("Don't have user email");
+    }
+/*
+Falsy Values
+false , 0, -0, BigInt, 0n , "" , null , undefined , NaN
+
+Truthy Values
+true, "0", 'false', " ", [], {}, function(){}
+
+//To chech array
+const userEmail=[]
+if (userEmail.length===0) {
+    console.log("Array is Empty");
+}
+
+//To chech objext
+const emptyObj={}
+if (Object.keys(emptyObj).length ===0) {
+    console.log("Object is Empty");
+}
+*/
